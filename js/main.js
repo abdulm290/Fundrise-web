a8eb37db545aad71cd11629dad8aa029f80493f5
Abdul Malik <ammalik290@gmail.com>
2021-09-12 09:11:54 +0600

    Fundrise website

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+    AOS.init();
+
+
+
+
+ $(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('shrink');
+  } else {
+    $('nav').removeClass('shrink');
+  }
+});
+
+
+
+
+ $(window).scroll(function () {
+            if ($(document).scrollTop() > 50) {
+                $('.nav').addClass('affix');
+                console.log("OK");
+            } else {
+                $('.nav').removeClass('affix');
+            }
+        });
+
+
+
+        $('.navTrigger').click(function () {
+            $(this).toggleClass('active');
+            console.log("Clicked menu");
+            $("#mainListDiv").toggleClass("show_list");
+            $("#mainListDiv").fadeIn();
+
+        });
+        
